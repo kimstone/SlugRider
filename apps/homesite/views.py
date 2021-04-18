0442069bb4eb345abae38c9003a8a0f7783a0277
Kim Stone <techmate@Mac-mini.localdomain>
2021-04-18 15:22:44 -0400

    completed configs for structure of landing page

--- apps/homesite/views.py
+++ apps/homesite/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
-# Create your views here.
+def index_view(request):
+    context = {}
+    context['say-what'] = "Welcome to the BEAST zone."
+    return render(request, 'homesite/home.html', context)
