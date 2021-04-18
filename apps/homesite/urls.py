35d35d29801f71761570daa4610fccf1e1ccdfa2
Kim Stone <techmate@Mac-mini.localdomain>
2021-04-18 19:35:36 -0400

    installed and configured bootstrap4; set-up the page structure for project

--- apps/homesite/urls.py
+++ apps/homesite/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index_view, name="home/index"),
+    path('', views.index_view, name="index"),
+    path('credits', views.credits_view, name="credits"),
+    path('login', views.login_view, name="login"),
+    path('logout', views.logout_view, name="logout"),
+    path('riders', views.riders_view, name="riders"),
+    path('register', views.register_view, name="register"),
+    path('profile', views.edit_profile_view, name="profile"),
 ]
